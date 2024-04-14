824ea487c69f5a060cc13ab16f6edb35339d0b5f
Haoyi Zheng <haoyi@umich.edu>
2024-04-14 15:42:06 -0400

    .

--- pre-processing.R
+++ pre-processing.R
@@ -1,3 +1,8 @@
 library(readxl)
-df <- read_xlsx('Fulldata_620W24_Project2.xlsx',sheet = 1)
+
 bs <- read_xlsx('Fulldata_620W24_Project2.xlsx',sheet = 2)
+
+full_data <- read_excel('Fulldata_620W24_Project2.xlsx', sheet = 1)
+selected_data <- full_data[c("pseudo_ID", "Date", "Total.ST", "Social.ST", "Pickups")]
+
+
